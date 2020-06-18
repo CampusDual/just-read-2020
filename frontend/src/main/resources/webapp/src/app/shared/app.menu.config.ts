9a3c945682be14d58f31931a58a921817178a34e
TaniaLinaresBoo <tania.linares@campusdual.com>
2020-06-18 17:35:16 +0200

    make html design

--- frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
+++ frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
@@ -2,5 +2,6 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
+  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' },
+  { id: 'books', name: 'BOOKS', icon: 'mybooks', route: '/main/books'}
 ];
