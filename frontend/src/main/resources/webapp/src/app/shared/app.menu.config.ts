d0e9b68ca061105831bcfb10ab7de88a7e134c43
LuisVaqueiroImatia <luis.vaqueiro@campusdual.com>
2020-06-19 13:59:58 +0200

    minor html changes

--- frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
+++ frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
@@ -3,5 +3,6 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' },
+  { id: 'searchResult', name: 'searchResults', route: '/main/search-results', icon: 'searchResults' },
   { id: 'books', name: 'BOOKS', icon: 'mybooks', route: '/main/books'}
 ];
