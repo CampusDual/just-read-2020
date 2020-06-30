d8f9f0f048d750b9803f33b0d56a7c8a2e183caa
LuisVaqueiroImatia <luis.vaqueiro@campusdual.com>
2020-06-30 13:41:32 +0200

    setted authors-detail routing and a new layout for authors-explore html

--- frontend/src/main/ngx/src/app/main/authors/authors.module.ts
+++ frontend/src/main/ngx/src/app/main/authors/authors.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import {OntimizeWebModule} from 'ontimize-web-ngx';
 import { AuthorsRoutingModule } from './authors-routing.module';
 import { AuthorsExploreComponent } from './authors-explore/authors-explore.component';
-
+import { AuthorsDetailComponent } from './authors-detail/authors-detail.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -11,7 +11,8 @@ import { AuthorsExploreComponent } from './authors-explore/authors-explore.compo
     OntimizeWebModule
   ],
   declarations: [
-    AuthorsExploreComponent
+    AuthorsExploreComponent,
+    AuthorsDetailComponent
   ]
 })
 export class AuthorsModule { }
