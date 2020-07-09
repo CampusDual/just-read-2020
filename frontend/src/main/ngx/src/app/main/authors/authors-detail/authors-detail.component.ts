841c8f84bfcfb58bd2eb9d244d6e5896f6c3acbe
Eduardo Barbosa Tarrío <eduardob907@gmail.com>
2020-07-09 15:23:16 +0200

    wip author details, changed main page, solved minor errors

--- frontend/src/main/ngx/src/app/main/authors/authors-detail/authors-detail.component.ts
+++ frontend/src/main/ngx/src/app/main/authors/authors-detail/authors-detail.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
+import { ApiService } from "app/api.service";
+import { DomSanitizer } from "@angular/platform-browser";
+import { Authors, AuthorBooks } from "app/model/author";
+import { loadAuthorsModule } from "app/main/main-routing.module";
 
 @Component({
-  selector: 'app-authors-detail',
-  templateUrl: './authors-detail.component.html',
-  styleUrls: ['./authors-detail.component.scss']
+  selector: "app-authors-detail",
+  templateUrl: "./authors-detail.component.html",
+  styleUrls: ["./authors-detail.component.scss"],
 })
 export class AuthorsDetailComponent implements OnInit {
+  authorId: number;
+  author: Authors;
+  books: AuthorBooks;
 
-  constructor() { }
+  private routeSub: Subscription;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private api: ApiService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit() {
+    this.routeSub = this.activatedRoute.params.subscribe((params) => {
+      this.authorId = Number(params["author_id"]);
+    });
+    this.loadAuthor();
   }
 
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
+  }
+
+  loadAuthor() {
+    this.api.getAuthorById(this.authorId).subscribe((data) => {
+      this.author = data;
+      this.api.getAuthorBooks(this.authorId).subscribe((data) => {
+        this.books = data;
+      });
+    });
+  }
+
+  getImageSrc(image: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      "data:image/jpg;base64," + image
+    );
+  }
 }
